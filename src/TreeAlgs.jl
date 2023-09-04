ea72827c9453e162b308cd06c621e0544e8c5ec7
PierreBarrat <p.barrat@live.fr>
2023-09-04 10:56:04 +0200

    remove compatibilitytree for now

--- src/TreeAlgs.jl
+++ src/TreeAlgs.jl
@@ -7,7 +7,7 @@ export LBI
 
 
 include("Sequences/Sequences.jl")
-include("CompatibilityTree/CompatibilityTree.jl")
+# include("CompatibilityTree/CompatibilityTree.jl")
 include("Evolve/Evolve.jl")
 include("Fitch/Fitch.jl")
 include("LBI/LBI.jl")
